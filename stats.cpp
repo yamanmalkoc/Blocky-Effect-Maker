0a7111ca7e6de201b293eaf9b4f493ba4a9647a8
v7i2b <v7i2b>
2019-03-30 18:33:43 -0700

    implemented one function rectArea

--- stats.cpp
+++ stats.cpp
@@ -2,18 +2,37 @@
 #include "stats.h"
 
 stats::stats(PNG & im){
-
+    // initialize all private vectors so that, for each color channel, 
+	// (x,y) is the cumulative sum of the the color values from (0,0)
+	// to (x,y), and the hist structure is as defined above.
+    // Note that the hue (h) value of each pixel is represented by
+    // its cartesian coordinates: X = s*cos(h) and Y = s*sin(h). 
+    // This is done to simplify distance computation. We calculate
+    // the cumulative sums for X and Y separately, and then combine
+    // them when we are doing color difference computation.
 /* your code here */
 
 }
 
 long stats::rectArea(pair<int,int> ul, pair<int,int> lr){
-
 /* your code here */
+    long xDiff = lr.first - ul.first;
+    long yDiff = lr.second - ul.second; 
+    return XDiff*yDiff; 
 
 }
 
 HSLAPixel stats::getAvg(pair<int,int> ul, pair<int,int> lr){
+	// given a rectangle, return the average color value over the rect.
+	/* Each color component of the pixel is the average value of that 
+	* component over the rectangle.
+	* @param ul is (x,y) of the upper left corner of the rectangle 
+	* @param lr is (x,y) of the lower right corner of the rectangle */
+    // The average hue value can be computed from the average X and
+    // Y values using the arctan function. You should research the 
+    // details of this. Finally, please set the average alpha channel to 
+    // 1.0.
+    HSLAPixel * pixel = getPixel(unsigned int x, unsigned int y);
 
 /* your code here */
 }
