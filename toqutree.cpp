536b74ba6cdc18f125f6b469c0ab0d04e774b0e1
Yaman Malko? <h0v1b@anvil.ugrad.cs.ubc.ca>
2019-04-04 16:01:07 -0700

    Children print nodes fixed

--- toqutree.cpp
+++ toqutree.cpp
@@ -38,13 +38,8 @@ toqutree::toqutree(PNG & imIn, int k){
 /* that imIn is large enough to contain an image of that size. */
 
 /* your code here */
-printf("YOOOOOOOOO");
-	//root->dimension = k;
-	printf("Yaman1");
 	int centerX = imIn.width() / 2;
-	printf("Yaman2");
 	int centerY = imIn.height() / 2;
-	printf("centerX: %i, centerY %i \n", centerX, centerY);
 
 	pair<int,int> ul;
 	ul.first = centerX - pow(2, k - 1);
@@ -218,21 +213,21 @@ toqutree::Node * toqutree::buildTree(PNG * im, int k) {
 		SWleaf.NE = NULL;
 		SWleaf.NW = NULL;
 		printf("\nNew leaf is created! The info of this leaf is:\n");
-		printNode(&SEleaf);
+		printNode(&SWleaf);
 
 		NEleaf.SE = NULL;
 		NEleaf.SW = NULL;
 		NEleaf.NE = NULL;
 		NEleaf.NW = NULL;
 		printf("\nNew leaf is created! The info of this leaf is:\n");
-		printNode(&SEleaf);
+		printNode(&NEleaf);
 
 		NWleaf.SE = NULL;
 		NWleaf.SW = NULL;
 		NWleaf.NE = NULL;
 		NWleaf.NW = NULL;
 		printf("\nNew leaf is created! The info of this leaf is:\n");
-		printNode(&SEleaf);
+		printNode(&NWleaf);
 
 		Node * retPointer = &last_parent;
 
